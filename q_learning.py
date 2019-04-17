10ec0307a1495ff596aad6824a0b233303cb84e8
Porter Jenkins <porterjenkins@gmail.com>
2019-04-17 13:40:41 -0400

    Generalize q-learning code: enable inheritance for linear and non-linear approximations

--- q_learning.py
+++ q_learning.py
@@ -2,41 +2,15 @@ import numpy as np
 import math
 import pickle
 
-class TabularQLearning(object):
+class QLearner(object):
+    def __init__(self, gamma, alpha):
+        self.gamma = gamma # discount factor
+        self.alpha = alpha # learning rate
 
-    def __init__(self, n_states, min_state_val, max_state_val, gamma=.5, alpha=.1):
-
-        self.min_state_val = min_state_val
-        self.max_state_val = max_state_val
-        self.n_states = n_states
-        self.state_buckets = np.linspace(min_state_val, max_state_val, n_states, endpoint=False)
-        self.bin_width = self.state_buckets[1] - self.state_buckets[0]
         self.actions = [.25, .2, .15, .1, .05, 0, -.25, -.2, -.15, -.1, -.05]
-        #self.actions = np.linspace(-.05,.05,21)
-
-        self.idx_action_map = dict(zip(range(len(self.actions)), self.actions))
-        self.action_idx_map = dict(zip(self.actions, range(len(self.actions))))
-
-
-        #self.q_table = np.random.randn(n_states, len(self.actions))
-        self.q_table = np.random.uniform(-10, 0, (n_states, len(self.actions)))
-
-        self.gamma = gamma
-        self.alpha = alpha
-
-
-    def get_state_idx(self, s):
-
-        state_idx = (s - self.min_state_val) // self.bin_width
-        return int(state_idx)
-
 
     def get_greedy_action(self, s):
-
-        state_idx = self.get_state_idx(s)
-        action_idx = np.argmax(self.q_table[state_idx, :])
-
-        return self.idx_action_map[action_idx]
+        pass
 
 
     def get_random_action(self):
@@ -54,29 +28,67 @@ class TabularQLearning(object):
 
         return action
 
+
+    def dump_model(self):
+        pass
+
+    def load_model(self, fname):
+        pass
+
+    def get_q_max(self, s):
+        pass
+
+
+
+
+class TabularQLearning(QLearner):
+
+    def __init__(self, n_states, min_state_val, max_state_val, gamma=.5, alpha=.1):
+        super().__init__(gamma, alpha)
+        self.min_state_val = min_state_val
+        self.max_state_val = max_state_val
+        self.n_states = n_states
+        self.state_buckets = np.linspace(min_state_val, max_state_val, n_states, endpoint=False)
+        self.bin_width = self.state_buckets[1] - self.state_buckets[0]
+
+        self.idx_action_map = dict(zip(range(len(self.actions)), self.actions))
+        self.action_idx_map = dict(zip(self.actions, range(len(self.actions))))
+
+        self.q_func = np.random.uniform(-10, 0, (n_states, len(self.actions)))
+
+        self.gamma = gamma
+        self.alpha = alpha
+
+
+    def get_state_idx(self, s):
+
+        state_idx = (s - self.min_state_val) // self.bin_width
+        return int(state_idx)
+
+    def get_greedy_action(self, s):
+
+        state_idx = self.get_state_idx(s)
+        action_idx = np.argmax(self.q_func[state_idx, :])
+
+        return self.idx_action_map[action_idx]
+
+
+
     def get_q_max(self, s):
 
         state_idx = self.get_state_idx(s)
-        return np.max(self.q_table[state_idx, :])
+        return np.max(self.q_func[state_idx, :])
 
 
     def update_q_value(self, s, a, r, q_max):
         state_idx = self.get_state_idx(s)
         action_idx = self.action_idx_map[a]
 
-        curr_q = self.q_table[state_idx, action_idx]
-        self.q_table[state_idx, action_idx] = curr_q + self.alpha*(r + self.gamma*q_max - curr_q)
-        new_q = self.q_table[state_idx, action_idx]
+        curr_q = self.q_func[state_idx, action_idx]
+        self.q_func[state_idx, action_idx] = curr_q + self.alpha*(r + self.gamma*q_max - curr_q)
+        new_q = self.q_func[state_idx, action_idx]
 
 
         print("Q-value update ({:.4f}, {:.2f}): {:.4f} --> {:.4f}".format(s, a, curr_q, new_q))
 
 
-
-    def dump_model(self):
-        with open('models/q-table.p', 'wb') as f:
-            pickle.dump(self.q_table, f)
-
-    def load_model(self, fname):
-        with open(fname, 'rb') as f:
-            self.q_table = pickle.load(f)
